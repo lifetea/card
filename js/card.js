75329ae59a8dea934e48c32450e237321ac16073
lifetea <494886251@qq.com>
2015-02-08 07:56:57 +0000

    update

--- js/card.js
+++ js/card.js
@@ -1,13 +1,10 @@
-﻿var scenes = [];
-var file = null;
+﻿var file = null;
 
 var canvas = null;
 var ctx = null;
 var cWidth = 0;
 var cHeight = 0;
 
-var pWidth = 0;
-var pHeight = 0;
 var pLeft = 0;
 var pTop = 0;
 
@@ -15,14 +12,22 @@ var mc = null;
 var image = null;
 var zoom = 1;
 
+var textarea = null;
+var textwrap = null;
+
+window.onload = function() { 
+	init();
+}
+
 function init(){
-	canvas = document.getElementById("myCanvas");
-    ctx = canvas.getContext("2d");
-	cWidth = canvas.width;
-	cHeight = canvas.height;
+	var container = document.getElementsByClassName('container')[0];
+	cWidth = container.offsetWidth;
+	cHeight = container.offsetHeight * 0.6;
 	
-	pWidth = cWidth;
-	pHeight = cHeight;
+	canvas = document.getElementById("myCanvas");
+	canvas.width = cWidth;
+	canvas.height = cHeight;
+    ctx = canvas.getContext("2d");
 	
 	var hitarea = document.getElementById('hitarea');
 	mc = new Hammer(hitarea);	
@@ -35,7 +40,6 @@ function init(){
 	mc.add([pinch]);
 	
 	image = document.getElementById('image');
-	scenes = document.getElementsByClassName('scene');
 }
 
 function selectPhoto(){
@@ -44,9 +48,6 @@ function selectPhoto(){
 	var fReader = new FileReader();
 	fReader.onloadend = function(e){
 		image.src = e.target.result;
-		for(var i = 0, len = scenes.length; i < len; i++){
-			scenes[i].className = 'scene opacity';
-		}
 	}
 	file = photofile.files[0];
 	fReader.readAsDataURL(file);
@@ -60,11 +61,15 @@ function initPhoto(){
 	zoom = 1;
 	
 	image.onload = function(){
-		mc.on("tap", function(ev) {
-			for(var i = 0, len = scenes.length; i < len; i++){
-				scenes[i].className = 'scene opacity';
-			}
-		});
+		document.getElementById('scene').className = 'hide';
+		document.getElementById('hitarea').className = '';
+		document.getElementById('bingo').className = '';
+		
+		if(image.offsetWidth > cWidth){
+			zoom = zoom * cWidth / image.offsetWidth;
+			image.style.width = cWidth +'px';
+		}
+		
 		/*移动*/
 		var top = 0;
 		var left = 0;	
@@ -81,7 +86,7 @@ function initPhoto(){
 		});
 		/*缩放*/
 		mc.on("pinchin", function(ev) {
-			if(image.offsetWidth > cWidth){
+			if(image.offsetWidth > 200){
 				image.style.width = image.offsetWidth * 0.98 +'px';
 				zoom = zoom * 0.98;
 			}
@@ -93,10 +98,26 @@ function initPhoto(){
 	};
 }
 
-function preview(){
-	for(var i = 0, len = scenes.length; i < len; i++){
-		scenes[i].className = 'scene';
-	}
+function clipPhoto(){
+	document.getElementById('scene').className = '';
+	document.getElementById('hitarea').className = 'hide';
+	document.getElementById('bingo').className = 'hide';
+}
+
+function initText(){
+	textarea = document.getElementById('textarea');
+	textwrap = document.getElementById('textwrap');
+	var textmc = new Hammer(textwrap);	
+	textmc.on("tap", function(ev) {
+		textarea.style.display = 'block';
+	});
+}
+function preview2(){
+	if(textarea == null) return false;
+	if(textwrap == null) return false;
+	
+	textarea.style.display = 'none';
+	textwrap.childNodes[0].innerHTML = textarea.value;
 }
 
 function upload(){
@@ -106,16 +127,35 @@ function upload(){
 	}
 	if(!confirm('你确定要分享吗？')) return false;
 	
-	/*获得场景的id(scene1|scene2|scene3)*/
-	var id = document.getElementsByClassName('swiper-slide-active')[0].id;
-	
 	/*获截图*/
 	ctx.clearRect(0, 0, cWidth, cHeight);
-    ctx.drawImage(image, -pLeft/zoom, -pTop/zoom, pWidth/zoom, pHeight/zoom, 0, 0, cWidth, cHeight);
+	/*计算截图数据*/
+	var il = pLeft > 0 ? 0 : -pLeft/zoom;
+	var it = pLeft > 0 ? 0 : -pTop/zoom;
+	
+	var cl = pLeft > 0 ? pLeft : 0;
+	var ct = pTop > 0 ? pTop : 0;
+	
+	var w = image.offsetWidth;
+	var h = image.offsetHeight;
+	
+	var pw = 0;
+	if(pLeft < 0){
+		pw = (w + pLeft) < cWidth ? (w + pLeft) : cWidth;
+	}else{
+		pw = (w + pLeft) < cWidth ? w : cWidth - pLeft;
+	}
+	
+	var ph = h > cHeight ? cHeight : h;
+	
+	ctx.drawImage(image, il, it, pw/zoom, ph/zoom, cl, ct, pw, ph);
 	
 	var dataURL = canvas.toDataURL();
 	//这个就是截图的dataURL
-	alert(dataURL);
+	//alert(dataURL);
+	//你可以用下面这句看到截图后的图片
+	//document.body.innerHTML = '<img src="'+dataURL+'" />';
+
 	return false;
 	
 	var xhr = new XMLHttpRequest();
