c816aa6cb2eae5fb14dd5b37791e71003fffab97
lifetea <494886251@qq.com>
2015-02-23 05:52:34 +0000

    安居客留言

--- card4/Home/Controller/IndexController.class.php
+++ card4/Home/Controller/IndexController.class.php
@@ -11,13 +11,20 @@ class IndexController extends Controller {
     	//echo $jssdk->hello();
     	$this->assign('data',$signPackage);
     	$pic = '';
+    	$val = '';
     	if($_GET['id']){
     		$pic ="http://card.greenco.com.cn/uploads/avatar/". $_GET['id'].".jpg";
     	}else{
     		$pic = "res/images/who.jpg";
     	}
+    	if($_GET['val']){
+    		$val = urldecode ($_GET['val']); 
+    	}else{
+    		$val = "新年快乐！恭喜发财！健康快乐";
+    	}
     	session('id',time());
     	$this->assign('id',session('id'));
+    	$this->assign('val',$val);
     	$this->assign('pic',$pic);
 		$this->display();
     }
